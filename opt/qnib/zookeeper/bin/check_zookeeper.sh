22f6a3db6076895f47689eb8e6ce5d2c730709ad
Christian Kniep <ckniep@gaikai.com>
2015-08-10 16:51:23 -0700

    make check more robust

--- opt/qnib/zookeeper/bin/check_zookeeper.sh
+++ opt/qnib/zookeeper/bin/check_zookeeper.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
 function set_zk_mode {
-	if ! egrep "tags.*${1}" /etc/consul.d/zookeeper.json;then
-        sed -i'' -e "s/tags\": .*/tags\": [\"${1}\"],/" /etc/consul.d/zookeeper.json
-        echo "Change tag to ${1}; reload consul"
+    mode=${1-standalone}
+    if [ ${mode} == "standalone" ];then
+        mode=leader
+   fi
+    if ! egrep "tags.*${mode}" /etc/consul.d/zookeeper.json;then
+        sed -i'' -e "s/tags\": .*/tags\": [\"${mode}\"],/" /etc/consul.d/zookeeper.json
+        echo "Change tag to ${mode}; reload consul"
         consul reload
-	fi
+    fi
 }
 
+## In case the zookeeper instance should not be started
+# eg. in case kafka inherits from this image, but uses an external zookeeper
+if [ "X${START_ZOOKEEPER}" == "xfalse" ];then
+    echo "'START_ZOOKEEPER==false', therefore it's fine... exit 2"
+    exit 2
+fi
+
 zk_stat=$(echo stat | nc 127.0.0.1 2181)
+if [ $? -ne 0 ];then
+    echo "No connection to localhost:2181"
+    exit 2
+fi
 mode=$(echo "${zk_stat}" |grep Mode |awk -F\: '{print $2}')
 if [ "X${mode}" == "" ];then
         echo "Checked status endpoint; couldn't find mode"
@@ -17,4 +32,5 @@ if [ "X${mode}" == "" ];then
 else
     set_zk_mode $(echo ${mode})
     echo "${zk_stat}"
+    exit 0
 fi
